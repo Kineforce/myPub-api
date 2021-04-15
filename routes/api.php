aac95a1b4170c30bb418d01a93df45dbd1a05771
kineforce <lucas-sm2010@hotmail.com>
2021-04-14 23:16:22 -0300

    Login e logout com token

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Clients;
+use App\Http\Controllers\AuthController;
 use App\Models\Client;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +20,15 @@ use App\Models\Client;
 
 
 // Public Routes
+Route::post('/register', [AuthController::class, 'register'] );
+Route::post('/login', [AuthController::class, 'login'] );
 Route::get('/clients', [Clients::class, 'index'] );
 Route::get('/clients/{id}', [Clients::class, 'show']);
 Route::get('/clients/search/{name}', [Clients::class, 'search']);
+
+
+
+// Unauthorized
 Route::get('login', function(){
     return "You need to auth!";
 })->name('login');
@@ -30,6 +38,7 @@ Route::group( ['middleware' => ['auth:sanctum']], function(){
     Route::post('/clients', [Clients::class, 'store'] );
     Route::put('/clients/{id}', [Clients::class, 'update'] );
     Route::delete('/clients/{id}', [Clients::class, 'destroy'] );
+    Route::post('/logout', [AuthController::class, 'logout'] );
 
 });
 
