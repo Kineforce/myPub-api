ac9f44c7de36e7b7545ad394b5a3d1c7cbade88e
kineforce <lucas-sm2010@hotmail.com>
2021-04-14 22:28:01 -0300

    Ajustes na API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Clients;
+use App\Models\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +16,24 @@ use App\Http\Controllers\Clients;
 |
 */
 
-Route::resource('clients', Clients::class);
-Route::get('/clients/search/{name}', [Clients::class, 'search'] );
 
-// Route::get('/getClients', [Clients::class, 'index'] );
-// Route::post('/setClient', [Clients::class, 'store']);
+// Public Routes
+Route::get('/clients', [Clients::class, 'index'] );
+Route::get('/clients/{id}', [Clients::class, 'show']);
+Route::get('/clients/search/{name}', [Clients::class, 'search']);
+Route::get('login', function(){
+    return "You need to auth!";
+})->name('login');
+
+// Protected Routes
+Route::group( ['middleware' => ['auth:sanctum']], function(){
+    Route::post('/clients', [Clients::class, 'store'] );
+    Route::put('/clients/{id}', [Clients::class, 'update'] );
+    Route::delete('/clients/{id}', [Clients::class, 'destroy'] );
+
+});
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
