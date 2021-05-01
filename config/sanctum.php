eb3349f39737363eb97762ba6400868f9d48841b
kineforce <lucas-sm2010@hotmail.com>
2021-05-01 14:54:56 -0300

    Teste prod

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'mypub-webmanager-api.herokuapp.com,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,'.parse_url(env('APP_URL'), PHP_URL_HOST)
+        'mypub-webmanager.herokuapp.com'.parse_url(env('APP_URL'), PHP_URL_HOST)
     )),
 
     /*
