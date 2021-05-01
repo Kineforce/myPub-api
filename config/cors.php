9ad6de783a5f16d1282bdd4ac832710302b6be7c
kineforce <lucas-sm2010@hotmail.com>
2021-05-01 14:40:30 -0300

    teste prod

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    //'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://mypub-webmanager.herokuapp.com'],
 
-    'allowed_origins' => ['*'],
+    //'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
